8e351ed57c3b2ae2c6c26254bbe72a4a5e912f40
skywindzz <skywindzz@yahoo.com>
2017-01-05 23:22:49 -0800

    1/5

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const router = require('./router');
 const mongoose = require('mongoose');
 
 //DB setup
-mongoose.connect('mongodb://localhost:auth/auth');
+mongoose.connect('mongodb://localhost:27017/auth');
 
 //App setup
 app.use(morgan('combined')); //login incoming request
