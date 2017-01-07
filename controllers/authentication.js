a220e75442c9db9e02a35ee4ae674c78b4b2605e
skywindzz <skywindzz@yahoo.com>
2017-01-06 23:50:39 -0800

    1/6

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -3,9 +3,12 @@ const User = require('../models/user');
 exports.signup = function(req, res, next) {
   const email = req.body.email;
   const password = req.body.password;
-
+  
+  if (!email || !password) {
+      return res.status(422).send ({ error: 'You must provide email and password'});
+  } 
   //see if a user with the given email exist
-
+ 
   //If a user with email does exist, return
   User.findOne({ email: email }, function(err, existingUser) {
     if (err) { return next(err); }
@@ -22,7 +25,7 @@ exports.signup = function(req, res, next) {
     user.save(function(err) {
       if(err) { return next(err); }
       //Respond to request indicating the user was created
-      res.json(user);
+      res.json({ success: true });
     });
   });
 }
